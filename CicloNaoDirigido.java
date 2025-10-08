30983137789b6e009ea4d5ad4ef91c8941ff52ce
Marcelo Cohen <mflashbr@gmail.com>
2025-10-08 20:26:05 -0300

    Detecção de ciclo não dirigido

--- CicloNaoDirigido.java
+++ CicloNaoDirigido.java
@@ -1,32 +1,56 @@
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class CicloNaoDirigido {
     private Set<String> marked; // vértices já visitados
+    private Set<String> edgeSet; // conj. arestas já usadas
+    private boolean hasCycle; // true se encontrou ciclo
 
     public CicloNaoDirigido(Graph g) {
         marked = new HashSet<>();
-        dfs(g, "0");
+        edgeSet = new HashSet<>();
+        hasCycle = false;
+        for(String v: g.getVerts()) {
+            if(!marked.contains(v))
+                System.out.println("Iniciando em "+v);
+                if(dfs(g, v)) // tem ciclo?
+                    break;
+        }
     }
 
-    private void dfs(Graph g, String v) {
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+
+    private boolean dfs(Graph g, String v) {
         System.out.println("Estou em " + v);
         marked.add(v); // marca v como visitado
-        for (String w : g.getAdj(v)) {
+        for (String u : g.getAdj(v)) {
             // Se w não está marcado, visita!
-            if (!marked.contains(w)) {
+            if (!marked.contains(u)) {
                 // Para chegar em w, vim de v
-                dfs(g, w);
+                edgeSet.add(u+"-"+v);
+                if(dfs(g, u) == true)
+                    return true;
+            }
+            else {
+                String aresta = v+"-"+u;
+                if(!edgeSet.contains(aresta)) {
+                    System.out.println("Achei um ciclo em "+aresta);
+                    hasCycle = true;
+                    return true;
+                }
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
         Graph g = new Graph("exemplos/tinyG.txt");
-        CicloNaoDirigido dfs = new CicloNaoDirigido(g);
+        CicloNaoDirigido detciclo = new CicloNaoDirigido(g);
+        if(detciclo.hasCycle())
+            System.out.println("Tem ciclo!");
+        else
+            System.out.println("Não tem ciclo...");
     }
 }
