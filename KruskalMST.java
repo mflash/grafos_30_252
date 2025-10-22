c5beb35325ad1d331f59a961a66ba8b84e41afca
Marcelo Cohen <mflashbr@gmail.com>
2025-10-22 20:38:58 -0300

    Exercício do mapa (implementação completa)

--- KruskalMST.java
+++ KruskalMST.java
@@ -4,26 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class KruskalMST {
-  
+
     private List<Edge> mst;
 
-    public KruskalMST(EdgeWeightedGraph g)
-    {
+    public KruskalMST(EdgeWeightedGraph g) {
         UnionFind uf = new UnionFind(g);
         mst = new LinkedList<>();
-        System.out.println("Total sets:"+uf.getTotalSets());
+        // System.out.println("Total sets:" + uf.getTotalSets());
         ArrayList<Edge> edges = new ArrayList<>();
-        for(Edge e: g.getEdges()) {
+        for (Edge e : g.getEdges()) {
             edges.add(e);
         }
         Collections.sort(edges);
-        for(Edge e: edges) {
-            System.out.println(e);
+        for (Edge e : edges) {
+            // System.out.println(e);
             String v = e.getV();
             String w = e.getW();
             String conj_v = uf.find(v);
             String conj_w = uf.find(w);
-            if(!conj_v.equals(conj_w)) {
+            if (!conj_v.equals(conj_w)) {
                 // Conj. diferentes => não forma ciclo!
                 // Adiciona aresta na árvore
                 mst.add(e);
@@ -41,8 +40,8 @@ public class KruskalMST {
     public static void main(String[] args) {
         EdgeWeightedGraph g = new EdgeWeightedGraph("exemplos/tinyEWG.txt");
         KruskalMST k = new KruskalMST(g);
-        for(Edge e: k.getMST()) {
-            System.out.println(">>> "+e);
+        for (Edge e : k.getMST()) {
+            System.out.println(">>> " + e);
         }
         System.out.println(g.toDot());
     }
--- Ponto.java
+++ Ponto.java
@@ -1,10 +1,23 @@
-public class Ponto {
+public class Ponto implements Comparable<Ponto> {
 
+    private static int numPontos = 0;
+    private int pos;
     private double x, y;
+    private double dist;
 
     Ponto(double x, double y) {
+        this.pos = numPontos;
         this.x = x;
         this.y = y;
+        numPontos++;
+    }
+
+    public static int getNumPontos() {
+        return numPontos;
+    }
+
+    public int getPos() {
+        return pos;
     }
 
     public double getX() {
@@ -15,6 +28,10 @@ public class Ponto {
         return y;
     }
 
+    public double getDist() {
+        return dist;
+    }
+
     public void setX(double x) {
         this.x = x;
     }
@@ -23,8 +40,26 @@ public class Ponto {
         this.y = y;
     }
 
+    public void setDist(double dist) {
+        this.dist = dist;
+    }
+
     @Override
     public String toString() {
-        return x + ", " + y;
+        return x + ", " + y + " (" + dist + ")";
+    }
+
+    public double calcDist(Ponto outro) {
+        return Math.sqrt(Math.pow(this.x - outro.x, 2) +
+                Math.pow(this.y - outro.y, 2));
+    }
+
+    @Override
+    public int compareTo(Ponto o) {
+        if (this.dist - o.dist > 0)
+            return 1;
+        else if (this.dist - o.dist < 0)
+            return -1;
+        return 0;
     }
 }
